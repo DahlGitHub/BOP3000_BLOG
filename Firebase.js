fa707a1ce9b090a6fc77d0cc10647cf89a13d28d
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-22 19:55:34 +0200

    minor changes

--- Firebase.js
+++ Firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABLxb0EvW1Nlpw8qOgaancU08CcCnv8eY",
-  authDomain: "hexacore-a379e.firebaseapp.com",
-  projectId: "hexacore-a379e",
-  storageBucket: "hexacore-a379e.appspot.com",
-  messagingSenderId: "322025778316",
-  appId: "1:322025778316:web:14cc77609c903374071fa9",
-  measurementId: "G-9RFZGP1RPS"
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
