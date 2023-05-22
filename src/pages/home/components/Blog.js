9f830882b14b882e7fb321e2d199f988000585e4
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2023-05-22 11:30:47 +0200

    reworked the content

--- src/pages/home/components/Blog.js
+++ src/pages/home/components/Blog.js
@@ -24,9 +24,9 @@ return (
             <BlogCard 
             icon={<i class="fa-solid fa-lightbulb fa-stack-1x text-success"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-success"></i>}
-            title="Planning" 
+            title="Ch. 1 | Bachelor Project Preparation" 
             date="Week 2 | Sprint 0" 
-            description="Our first bachelor meeting, listed down potential bachelor project concepts, desired frameworks, software tools, programming language etc. Made a group contract while trying to define  our project's scope." 
+            description="During our first bachelor meeting, we brainstormed potential project concepts, discussed desired frameworks, software tools, and programming languages. We also created a group contract to define the project's scope."
             author="Adrian"
             img_author="https://cdn.discordapp.com/attachments/745379181214433454/933000365685760030/unknown.png"
             />
@@ -35,31 +35,32 @@ return (
             icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
 
-            title="Bachelor Blog" 
+            title="Ch. 1.1 | Bachelor Blog" 
             date="Week 3 | Sprint 1" 
-            description="Website for the bachelor blog is up and running, here you'll find weekly sprint updates and important documents regarding the bachelor project." 
+            description="Our bachelor blog website is now up and running. Here, you'll find weekly sprint updates and access to important documents related to the bachelor project." 
             author="Adrian"
             img_author="https://cdn.discordapp.com/attachments/745379181214433454/933000365685760030/unknown.png"
             />
 
             <BlogCard 
-            icon={<i class="fa-solid fa-pencil fa-stack-1x text-warning"></i>}
-            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-warning"></i>}
+             icon={<i class="fa-solid fa-lightbulb fa-stack-1x text-success"></i>}
+             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-success"></i>}
+            
 
-            title="Sketch" 
+            title="Ch. 1.2 | Project Idea" 
             date="Week 4 | Sprint 1" 
-            description="Bachelor Project Idea has been choosen, the team is scrambling together use cases - preparing to write the actual sketch document for our bachelor." 
+            description="We have selected the bachelor project idea and are now busy gathering use cases. This will help us in preparing the actual sketch document for our bachelor project." 
             author="Adrian"
             img_author="https://cdn.discordapp.com/attachments/745379181214433454/933000365685760030/unknown.png"
             />
 
             <BlogCard 
-            icon={<i class="fa-solid fa-lightbulb fa-stack-1x text-success"></i>}
-            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-success"></i>}
+            icon={<i class="fa-solid fa-pencil fa-stack-1x text-warning"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-warning"></i>}
 
-            title="The Idea" 
+            title="Ch. 1.3 | Sketch Document" 
             date="Week 5 | Sprint 2" 
-            description="We started the week by delivering the sketch document for our bachelor. Which lead to us all discussing logos and a color scheme for our brand. Some of us even started looking and experimenting with technologies we want to use. We discovered that we need a simple wireframe to help us all get a vision of how the service will look." 
+            description="We began the week by finalizing and delivering the sketch document for our bachelor project. This led us to engage in discussions about logos and color schemes for our brand. Additionally, some team members started exploring and experimenting with the technologies we plan to use. During this process, we realized the need for a simple wireframe to visualize the service." 
             author="Jøran"
             img_author="https://cdn.discordapp.com/attachments/1021153022576959501/1065007817859268618/jran1.jpg"
             />
@@ -68,25 +69,20 @@ return (
             icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
 
-            title="The beginning" 
+            title="Ch. 2 | Tech Stack Research" 
             date="Week 6 | Sprint 2" 
-            description="The team has begun to research the tech stack, with steady progress. Both the frontend and the backend team has clear goals ahead for the sprint. 
-            
-            We have decided to scrap the SQL database and have now set our eyes on the hosting and NOSQL database service Firebase.
-            
-           
-            The frontend team has decided upon Next UI and Tailwind CSS as our design library. This is due to its compatibility and size when using NextJS. The login system is close to complete, and the application has been deployed."
-            author="Martin"
+            description="We have initiated research on the tech stack and are making steady progress. Both the frontend and backend teams have set clear goals for the sprint. After careful consideration, we decided to move away from the SQL database and focus on hosting and using Firebase as our NoSQL database service. For frontend design, we opted for Next UI and Tailwind CSS due to their compatibility and size advantages when using NextJS. The login system is nearing completion, and the application has been successfully deployed."            
+        author="Martin"
             img_author="https://cdn.discordapp.com/attachments/1064270341901389865/1064920855035007087/CV_bilde.jpg"
             />
 
             <BlogCard 
-            icon={<i class="fa-solid fa-bell fa-stack-1x text-primary"></i>}
-            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
+            icon={<i class="fa-solid fa-bell fa-stack-1x text-danger"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-danger"></i>}
 
-            title="Milestone 1" 
+            title="Ch. 2.1 | Milestone 1" 
             date="Week 7 | Sprint 2" 
-            description="We’ve finished milestone 1 presentation, explaining our project and our decisions around our project. We’ve begun looking at the realtime data which is essential for communication on our site. Our domain has been set up to https://hexacore.no/ for our application. "
+            description="We successfully completed the milestone 1 presentation, where we explained our project and the decisions we made. Furthermore, we started exploring the implementation of realtime data, which is essential for effective communication on our website."
             author="Henrik"
             img_author="https://cdn.discordapp.com/attachments/1062428707639275650/1066377474100957204/henrik.jpg"
             />
@@ -95,9 +91,9 @@ return (
             icon={<i class="fa-solid fa-pencil fa-stack-1x text-warning"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-warning"></i>}
 
-            title="Project Description" 
+            title="Ch. 3 | Project Description" 
             date="Week 8 | Sprint 2" 
-            description="We've finished the project description, file has been uploaded to the blog. The group is continuing to work on the application."
+            description="The project description has been completed, and the file has been uploaded to the blog. Our group is actively continuing the work on the application."
             author="Adrian"
             img_author="https://cdn.discordapp.com/attachments/745379181214433454/933000365685760030/unknown.png"
             />
@@ -106,20 +102,20 @@ return (
             icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
 
-            title="Core components" 
+            title="Ch. 3.1 | Core Components" 
             date="Week 9 | Sprint 2" 
-            description="This week we started work on implementing major functions such as a contact list, chat and general functionality to the layout. We also interviewed other students to figure out what features people want, the interviews gave us interesting viewpoints for us to reflect about."
+            description="During this week, we focused on implementing major functions such as a contact list, chat, and general functionality for the layout. Additionally, we conducted interviews with other students to gain insights into desired features. The interviews provided us with valuable viewpoints for reflection and improvement."
             author="Jøran"
             img_author="https://cdn.discordapp.com/attachments/1021153022576959501/1065007817859268618/jran1.jpg"
             />
 
             <BlogCard 
-            icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
-            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
+            icon={<i class="fa-solid fa-bell fa-stack-1x text-danger"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-danger"></i>}
 
-            title="Core components" 
+            title="Ch. 3.2 | Milestone 2" 
             date="Week 10 | Sprint 2" 
-            description="For this week the team has focused on developing core components for the application. We have been working on a searchable user registry, chat and navigational solutions. These features will be improved upon during the weekend."
+            description="We successfully completed the milestone 2 presentation. We focused on creating a searchable user registry, chat features, and navigational solutions. These features will be further enhanced during the upcoming weekend."
             author="Martin"
             img_author="https://cdn.discordapp.com/attachments/1064270341901389865/1064920855035007087/CV_bilde.jpg"
             />
@@ -128,9 +124,9 @@ return (
             icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
 
-            title="Core components V2" 
+            title="Ch. 3.3 | Progress on Features" 
             date="Week 11 | Sprint 3" 
-            description="We’ve progressed on the landing page. We’ve added data from user for chat. We’ve made a contact list for user. We’ve started making a kanban board. We’ve begun looking at creating groups"
+            description="We made significant progress on the landing page, incorporating user data for the chat feature and creating a contact list for users. Furthermore, we started building a kanban board and exploring the creation of groups."
             author="Henrik"
             img_author="https://cdn.discordapp.com/attachments/1062428707639275650/1066377474100957204/henrik.jpg"
             />
@@ -139,9 +135,9 @@ return (
             icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
 
-            title="Styling the components" 
+            title="Ch. 4 | Styling and Testing" 
             date="Week 12 | Sprint 3" 
-            description="Most of the functions have been styled accordingly, tested along with UX and some UI."
+            description="Most of the functions have been styled accordingly and thoroughly tested for a seamless user experience. We have also made some improvements to the user interface."
             author="Adrian"
             img_author="https://cdn.discordapp.com/attachments/745379181214433454/933000365685760030/unknown.png"
             />
@@ -150,9 +146,9 @@ return (
             icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
 
-            title="Testable Product" 
+            title="Ch. 4.1 | Main Feature Development" 
             date="Week 13 | Sprint 3" 
-            description="This week we made a lot of progress on our main features. we all had to put in a bit more work this week since we realized we were a bit behind schedule. the focus is producing a version that is testable by other users, so we can gather information and input of whats good and what needs to change."
+            description="We made substantial progress on our main features during this week. Realizing that we were slightly behind schedule, we collectively put in extra effort to ensure that we deliver a testable version to gather user feedback. This feedback will help us identify areas of improvement and necessary changes."
             author="Jøran"
             img_author="https://cdn.discordapp.com/attachments/1021153022576959501/1065007817859268618/jran1.jpg"
             />
@@ -161,24 +157,86 @@ return (
             icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
 
-            title="Preperations" 
+            title="Ch. 4.2 | Preperations" 
             date="Week 14 | Sprint 3" 
-            description="The team has continued working on tasks such as the chat and the kanban board. We will focus on implementing important tools to the project, and prepare for the upcoming Milestone 3 presentation."
+            description="The team has been diligently working on tasks related to the chat feature and the kanban board. Additionally, we are focused on implementing important tools and preparing for the upcoming Milestone 3 presentation."
             author="Martin"
             img_author="https://cdn.discordapp.com/attachments/1064270341901389865/1064920855035007087/CV_bilde.jpg"
+            />
+
+            <BlogCard 
+            icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
+
+            title="Ch. 5 | Polishing" 
+            date="Week 15 | Sprint 4" 
+            description="We have successfully polished the user settings, files management, and kanban board functionalities, ensuring both styling and functional accuracy as described in the project description."
+            author="Adrian"
+            img_author="https://cdn.discordapp.com/attachments/745379181214433454/933000365685760030/unknown.png"
+            />
+
+            <BlogCard 
+            icon={<i class="fa-solid fa-code fa-stack-1x text-primary"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-primary"></i>}
+
+            title="Ch. 5.1 | New Fuctionality" 
+            date="Week 16 | Sprint 4" 
+            description="During this phase, we introduced a poll system for voting in chats and implemented infinite scroll for better user experience. We also made adjustments to the team UI and added the option to invite team members. Additionally, we created a feature that allows users to create tools for their teams. While the video chat is temporarily on hold, we have initiated the development of a customer service feature."
+            author="Henrik"
+            img_author="https://cdn.discordapp.com/attachments/1062428707639275650/1066377474100957204/henrik.jpg"
+            />
+
+            <BlogCard 
+            icon={<i class="fa-solid fa-bell fa-stack-1x text-danger"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-danger"></i>}
+
+            title="Ch. 6 | Milestone 3" 
+            date="Week 17 | Sprint 4" 
+            description="During this week, we presented our bachelor project for the Milestone 3 presentation. As we had another upcoming exam, we took a short break from the project. Unfortunately, just before the presentation, we encountered a few new and frustrating bugs and issues in our project. Our focus for the next week will be to address these issues and begin working on the report."
+            author="Jøran"
+            img_author="https://cdn.discordapp.com/attachments/1021153022576959501/1065007817859268618/jran1.jpg"
             />
 
             <BlogCard 
             icon={<i class="fa-solid fa-pencil fa-stack-1x text-warning"></i>}
             iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-warning"></i>}
 
-            title="Design Finalization" 
-            date="Week 15 | Sprint 3" 
-            description="User settings, files management and kanban have been polished with styling and is functionally working accordingly as intented in the project description."
+            title="Ch. 6.1 | Issues and Report" 
+            date="Week 18 | Sprint 5" 
+            description="After resolving the newly discovered issues, we dedicated our efforts to fixing them. Subsequently, we started structuring and writing the report. Despite the small delay from the previous week, things are progressing well and according to schedule."
+            author="Jøran"
+            img_author="https://cdn.discordapp.com/attachments/1021153022576959501/1065007817859268618/jran1.jpg"
+            />
+            <BlogCard 
+            icon={<i class="fa-solid fa-pencil fa-stack-1x text-warning"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-warning"></i>}
+
+            title="Ch. 6.2 | User Testing and Report" 
+            date="Week 19 | Sprint 5" 
+            description="Significant progress has been made on the project report during this week. While many existing bugs have been ironed out, we have also started user testing the application, which may potentially uncover unexpected issues or bugs."
+            author="Jøran"
+            img_author="https://cdn.discordapp.com/attachments/1021153022576959501/1065007817859268618/jran1.jpg"
+            />
+            <BlogCard 
+            icon={<i class="fa-solid fa-pencil fa-stack-1x text-warning"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-warning"></i>}
+
+            title="Ch. 6.3 | Optimizing" 
+            date="Week 20 | Sprint 5" 
+            description="We are diligently working on implementing solutions and addressing issues based on the feedback we have received from users. The report has shown incredible progress, and we are optimistic about presenting our results during the final presentation."
+            author="Jøran"
+            img_author="https://cdn.discordapp.com/attachments/1021153022576959501/1065007817859268618/jran1.jpg"
+            />
+                        <BlogCard 
+            icon={<i class="fa-solid fa-bell fa-stack-1x text-danger"></i>}
+            iconbg={<i class="fa-solid fa-diamond fa-stack-2x opacity-25 text-danger"></i>}
+
+            title="Ch. 7 | Completion and Submission" 
+            date="Week 21 | 22th May" 
+            description="We have successfully finished writing the bachelor report and submitted it. Currently, we are preparing for the final presentation and eagerly looking forward to showcasing our results."
             author="Adrian"
             img_author="https://cdn.discordapp.com/attachments/745379181214433454/933000365685760030/unknown.png"
             />
-
         </div>
             
         </div>
